01361fcd809662281f3bc62564af91350004d545
youssef <ykamel11@gmail.com>
2019-10-20 16:42:56 +0200

    HW

---
+++ HW s07-08prog1.py
@@ -0,0 +1,11 @@
+s = "azcbobobegghakl"
+longest = s[0]
+current = s[0]
+for c in s[1:]:
+    if c >= current[-1]:
+        current += c
+        if len(current) > len(longest):
+            longest = current
+    else:
+        current = c
+print("Longest substring in alphabetical order is:", longest)
