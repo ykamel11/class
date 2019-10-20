913bec22787b735ca3a502bfbb1ca800c1e95f2a
youssef <ykamel11@gmail.com>
2019-10-20 16:43:52 +0200

    HW

--- HW s05-06prog1.py
+++ HW s05-06prog1.py
@@ -7,8 +7,8 @@ play = True
 
 class LosingLife:
     if stepCounter == 10:
-        lifeCounter - 1
-        stepCounter = 0
+        lifeCounter == 3
+        stepCounter == 0
 
 
 class LifeChecker:
@@ -21,28 +21,41 @@ while play:
     print("You are in the magic maze. Which way to go? (N,S,E,W)")
     if input() == "S":
         print("you may continue")
+        stepCounter += 1
         if input() == "S":
             print("well done, you may continue")
+            stepCounter += 1
             if input() == "N":
                 print("well done, you may continue")
+                stepCounter += 1
                 if input() == "W":
                     print("well done, you may continue")
+                    stepCounter += 1
                     if input() == "E":
                         print("well done, you may continue")
+                        stepCounter += 1
                         if input() == "S":
                             print("well done, you have escaped the maze, you may leave")
+                            stepCounter += 1
+                            print("Steps: "+stepCounter)
                             play = False
 
                         else:
                             print("wrong move")
+                            stepCounter += 1
                     else:
                         print("wrong move")
+                        stepCounter += 1
                 else:
                     print("wrong move")
+                    stepCounter += 1
             else:
                 print("wrong move")
+                stepCounter += 1
         else:
             print("wrong move")
+            stepCounter += 1
 
     else :
         print("wrong move")
+        stepCounter += 1
